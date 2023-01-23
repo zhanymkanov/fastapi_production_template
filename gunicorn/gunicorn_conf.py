592f2a39b088e5b983882733f8e8d1e5dd9c24a6
Yerassyl <zhanymkanov@gmail.com>
2023-01-23 19:01:30 +0600

    Fix gunicorn configs to use stdout

--- gunicorn/gunicorn_conf.py
+++ gunicorn/gunicorn_conf.py
@@ -26,12 +26,6 @@ else:
         use_max_workers = int(max_workers_str)
         web_concurrency = min(web_concurrency, use_max_workers)
 
-accesslog_var = os.getenv("ACCESS_LOG", "stdout")
-use_accesslog = accesslog_var or None
-
-errorlog_var = os.getenv("ERROR_LOG", "stdout")
-use_errorlog = errorlog_var or None
-
 graceful_timeout_str = os.getenv("GRACEFUL_TIMEOUT", "120")
 timeout_str = os.getenv("TIMEOUT", "120")
 keepalive_str = os.getenv("KEEP_ALIVE", "5")
@@ -41,9 +35,7 @@ use_loglevel = os.getenv("LOG_LEVEL", "info")
 loglevel = use_loglevel
 workers = web_concurrency
 bind = use_bind
-errorlog = use_errorlog
 worker_tmp_dir = "/dev/shm"
-accesslog = use_accesslog
 graceful_timeout = int(graceful_timeout_str)
 timeout = int(timeout_str)
 keepalive = int(keepalive_str)
